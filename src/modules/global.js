29052b40525854852268d04f1d136e5c204809c4
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-04-13 11:32:41 -0400

    Setup global variables

--- src/modules/global.js
+++ src/modules/global.js
@@ -1,3 +1,14 @@
 class GlobalVariables {
-
+  static showsLink = document.getElementById('shows-link');
+  static showList = document.getElementById('show-list');
+  static popupImage = document.querySelector('.popup-image');
+  static popupTitle = document.querySelector('.popup-title');
+  static showDetails1 = document.querySelector('.info-1');
+  static showDetails2 = document.querySelector('.info-2');
+  static showDetails3 = document.querySelector('.info-3');
+  static showDetails4 = document.querySelector('.info-4');
+  static commentsContainer = document.querySelector('.comments-container');
+  static inputField = document.querySelector('.input-name');
+  static commentField = document.querySelector('.comment-details');
+  static submitButton = getElementById('submit');
 }
