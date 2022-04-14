7018a18cc331aca91d568a19816d7431d79fe57a
Dangelo Arrivillaga <talgidisoluciones@gmail.com>
2022-04-14 14:32:58 -0400

    Setup involmentAPI pt.2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './index.css';
 // import TVMazeAPI from './modules/TVMazeAPI';
 import PageManager from './modules/pageManager';
+import { addComments, loadComments } from './modules/involvementAPI';
 
 const pageManager = new PageManager();
 
@@ -11,3 +12,16 @@ const appManager = async () => {
 
 appManager();
 // pageManager.addCard();
+
+
+//////////////////////
+const form = document.getElementById('comment-form');
+
+loadComments();
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addComments();
+  loadComments();
+  form.reset();
+});
--- src/modules/involvementAPI.js
+++ src/modules/involvementAPI.js
@@ -1,2 +1,46 @@
-const commentsAPI_URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/GEEEq0hutMWJ97Wdtxcj';
+import GlobalVariables from './global';
 
+const commentsAPI_URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/GEEEq0hutMWJ97Wdtxcj';
+const commentList = document.getElementById('comment-list');
+const vg = new GlobalVariables();
+
+const createDiv = (comment) => {
+  commentList.innerHTML = '';
+  const sortedComments = comment.sort((a, b) => b.comment - a.comment);
+  sortedComments.forEach((comment) => {
+    const commentContainer = `
+      <li class="score-text">${comment.user}: ${comment.comment}</li>
+    `;
+    commentList.innerHTML += commentContainer;
+  });
+};
+// Comments
+const getComments = async () => {
+  const response = await fetch(commentsAPI_URL);
+  const comments = await response.json();
+  return comments.result;
+};
+
+const addComments = async () => {
+  const response = fetch(commentsAPI_URL, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      user: vg.inputField.value,
+      comment: vg.commentField.value,
+    }),
+  });
+
+  const status = await response.json();
+  return status;
+};
+
+const loadComments = () => {
+  getComments().then((comments) => {
+    createDiv(comments);
+  });
+};
+
+export { addComments, loadComments };
