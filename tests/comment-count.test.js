626f4c001629fd76fc06033d21752c578214943d
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-04-15 18:46:01 -0400

    Fix linter error

--- tests/comment-count.test.js
+++ tests/comment-count.test.js
@@ -5,7 +5,6 @@ import { commentData } from './mock-data';
 describe('Confirms the number of comments of the shows in the popup', () => {
   beforeEach(() => {
     document.body.innerHTML = fs.readFileSync('dist/index.html');
-    const commentList = document.getElementById('comment-list');
   });
   it('counts the number of comments elements from the involvement API data', async () => {
     const totalComments = involvementAPI.createDiv(commentData);
